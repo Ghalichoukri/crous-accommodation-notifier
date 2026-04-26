14969e9e862de6a17ac5622daedf51fea5946317
Ghalichoukri <63523187+Ghalichoukri@users.noreply.github.com>
2026-04-26 15:37:08 +0200

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -1,8 +1,5 @@
-# pydantic-settings class
-
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
@@ -11,11 +8,9 @@ class Settings(BaseSettings):
         env_file_encoding="utf-8",
         extra="ignore",
     )
-
     MSE_LOGIN_URL: str = "https://www.messervices.etudiant.gouv.fr/envole/oauth2/login"
-
     MSE_EMAIL: str = Field(default=...)
     MSE_PASSWORD: str = Field(default=...)
-
     TELEGRAM_BOT_TOKEN: str = Field(default=...)
     MY_TELEGRAM_ID: str = Field(default=...)
+    SEARCH_URL: str = Field(default=...)
